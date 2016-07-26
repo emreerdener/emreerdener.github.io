d4b5df15d4c29039a3982a0603c02013fd361554
emreerdener <emreerdener@Emres-MacBook-Pro.local>
2016-07-25 23:04:35 -0500

    rep queue issue fixed (thank you jay), fontAwesome added, navbar added, style changes, updates, etc

--- js/main.js
+++ js/main.js
@@ -1,40 +1,56 @@
 $(document).ready(function() {
  
+    var reps = [];
+    var queues = [];
+
+    
+//---------Navbar----------
+    //Adds spin animation to Settings "gear" in navbar 
+    $('.fa-cog').hover(function() {
+        $('.fa-cog').toggleClass('fa-spin');
+    });
+    
 //---------Settings---------    
     
     //Rep Working button & Rep Profile
     $('.add-rep').click(function() {
         //Name for Rep from input value. Then assigned to variable
-        $repName = $('#addrep-name').val();
+        repName = $('#addrep-name').val();
+        
+        //find way to block adding rep if has the same name.
+        //check if repName is in rep array. if so reject it.
+        //reps.indexOf(repName)
+        //then push to rep array      
         
         //Rep Button stored in local variable (to be added)
-        var repButton = '<button type="button" class="btn-rep btn btn-lg btn-default">' + $repName + '</button>';
+        var repButton = '<button type="button" class="btn-rep btn btn-lg btn-default">' + repName + '</button>';
         
         //Rep Profile stored in variable (to be added)
-        repProfile = '<div class="container rep-card"><h3 class="repProfileName">' + $repName + '</h3></div>';
+        repProfile = '<div class="container rep-card"><h3 class="repProfileName">' + repName + '</h3></div>';
         
         //Rep Profile added in rep-profile container
         $('.rep-profiles').prepend(repProfile);
         
         //Rep Button added in rep-buttons container
-        $('.rep-buttons').append(repButton);  
+        $('.rep-buttons').append(repButton);
         
-    });//close add rep
+        //Adds queues array to local variable of $repName
+        $('.rep-card:contains(' + repName + ')').append(queues);   
+    });//close add rep    
     
     
     //Queue button to Rep Profile
-    $('.add-queue').click(function() {
-        queues = [];
+    $('.add-queue').click(function() {    
         
         //Name for Queue from input value
         $queueName = $('#addQueue-name').val();       
         //Queue button stored in variable (to be added)
-        repQueues = '<button class="btn btn-lg btn-block btn-default repQueues">' + $queueName + '</button>';
+        repQueues = '<button class="btn btn-lg btn-block btn-default repQueues-btn">' + $queueName + '</button>';
         
         //Adds Queue button to array
         queues.push(repQueues);
         //Adds queues to Rep Profile card
-        $('.rep-card').append(queues);
+        $('.rep-card').append(repQueues);
     });//close add queue
         
     
